cd776a9529495173b313bcbffa99dbaa9bc8d326
Toby Harris <operator@tobyz.net>
2021-05-12 21:50:31 +0100

    'Hello world' status bar app

--- TypoToggle/AppDelegate.swift
+++ TypoToggle/AppDelegate.swift
@@ -11,23 +11,14 @@ import SwiftUI
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    var window: NSWindow!
-
+    var statusBar: StatusBarController?
+    var popover = NSPopover()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
-
-        // Create the window and set the content view.
-        window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
-            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
-            backing: .buffered, defer: false)
-        window.isReleasedWhenClosed = false
-        window.center()
-        window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
-        window.makeKeyAndOrderFront(nil)
+        popover.contentSize = NSSize(width: 360, height: 360)
+        popover.contentViewController = NSHostingController(rootView: contentView)
+        statusBar = StatusBarController(popover)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ TypoToggle/StatusBarController.swift
@@ -0,0 +1,47 @@
+//
+//  StatusBarController.swift
+//  TypoToggle
+//
+//  Created by Toby Harris on 12/05/2021.
+//
+
+import AppKit
+
+class StatusBarController {
+    private var statusBar: NSStatusBar
+    private var statusItem: NSStatusItem
+    private var popover: NSPopover
+    
+    init(_ popover: NSPopover) {
+        self.popover = popover
+        statusBar = NSStatusBar()
+        statusItem = statusBar.statusItem(withLength: 28.0)
+        
+        if let statusBarButton = statusItem.button {
+            statusBarButton.image = NSImage(systemSymbolName: "quote.bubble", accessibilityDescription: "Quotes")
+            statusBarButton.image?.size = NSSize(width: 18.0, height: 18.0)
+            statusBarButton.image?.isTemplate = true
+            statusBarButton.action = #selector(togglePopover(sender:))
+            statusBarButton.target = self
+        }
+    }
+    
+    @objc func togglePopover(sender: AnyObject) {
+        if popover.isShown {
+            hidePopover(sender)
+        }
+        else {
+            showPopover(sender)
+        }
+    }
+    
+    func showPopover(_ sender: AnyObject) {
+        if let statusBarButton = statusItem.button {
+            popover.show(relativeTo: statusBarButton.bounds, of: statusBarButton, preferredEdge: NSRectEdge.maxY)
+        }
+    }
+    
+    func hidePopover(_ sender: AnyObject) {
+        popover.performClose(sender)
+    }
+}
